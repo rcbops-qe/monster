984c2a06d77ca878417d62450263c58f2a6e3924
cjoelrun <cameronjlopez@gmail.com>
2013-10-20 16:48:00 -0500

    config log level and file

--- monster/util.py
+++ monster/util.py
@@ -1,7 +1,22 @@
 import logging
 
+# Log to console
 logger = logging.getLogger("rcbops.qa")
-logger.setLevel(logging.DEBUG)
 console_handler = logging.StreamHandler()
-console_handler.setLevel(logging.DEBUG)
+console_handler.setLevel(logging.INFO)
+format = '%(asctime)s %(name)s %(levelname)s: %(message)s'
+formatter = logging.Formatter(format)
+console_handler.setFormatter(formatter)
 logger.addHandler(console_handler)
+
+
+def set_log_level(level):
+    log_level = getattr(logging, level, logging.INFO)
+    logger.setLevel(log_level)
+
+
+def log_to_file(file):
+    fh = logging.FileHandler(file)
+    fh.setFormatter(console_handler.formatter)
+    fh.setLevel(logging.DEBUG)
+    logger.addHandler(fh)
