91e247f802c7fa2731ace92cf11f2105fb2a02b7
cjoelrun <cameronjlopez@gmail.com>
2013-10-23 11:47:25 -0500

    add secrets to config object

--- monster/Config.py
+++ monster/Config.py
@@ -14,6 +14,10 @@ class Config(object):
                                 'config.yaml')
         f = open(file)
         self.config = load(f)
+        # Add secrets to config object
+        secret_file = os.path.join(os.path.dirname(file), "secret.yaml")
+        secrets = load(secret_file)
+        self.config['secrets'] = secrets
 
     def __getitem__(self, name):
         return self.config[name]
