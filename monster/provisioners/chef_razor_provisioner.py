a2549a52425025fc0874671ab4abb906e0b1868d
cjoelrun <cameronjlopez@gmail.com>
2013-10-26 22:41:32 -0500

    fix imports

--- monster/provisioners/chef_razor_provisioner.py
+++ monster/provisioners/chef_razor_provisioner.py
@@ -1,4 +1,10 @@
+from time import sleep
+
+from chef import Node, Client
+
+from monster import util
 from monster.provisioners.provisioner import Provisioner
+from monster.razor_api import razor_api
 
 
 class ChefRazorPrzaself):
