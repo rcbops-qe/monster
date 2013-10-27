d52b9b9d89a42f009c644aae2b02c0c2db41798c
cjoelrun <cameronjlopez@gmail.com>
2013-10-26 22:49:40 -0500

    refactor nodes

---
+++ monster/nodes/chef_node.py
@@ -0,0 +1,115 @@
+import traceback
+from chef import Node as CNode
+from inspect import getmembers, isclass
+
+from monster import util
+from monster.nodes.node import Node
+from monster.features import node_features
+
+
+class ChefNode(Node):
+    """
+    A chef entity
+    Provides chef related server fuctions
+    """
+    def __init__(self, ip, user, password, os, product, environment,
+                 deployment, name, provisioner, branch, status="provisioning"):
+        super(ChefNode, self).__init__(ip, user, password, os, product,
+                                       environment, deployment, provisioner,
+                                       status)
+        self.name = name
+        self.branch = branch
+        self.run_list = []
+        self.features = []
+
+    def __str__(self):
+        features = ", ".join((str(f) for f in self.features))
+        node = ("Node - name:{0}, os:{1}, branch:{2}, ip:{3}, status:{4}\n\t\t"
+                "Features: {5}").format(self.name, self.os_name, self.branch,
+                                        self.ipaddress, self.status, features)
+        return node
+
+    def __getitem__(self, item):
+        """
+        Node has access to chef attributes
+        """
+        return CNode(self.name, api=self.environment.local_api)[item]
+
+    def __setitem__(self, item, value):
+        """
+        Node can set chef attributes
+        """
+        util.logger.debug("setting {0} to {1} on {2}".format(item, value,
+                                                             self.name))
+        lnode = CNode(self.name, api=self.environment.local_api)
+        lnode[item] = value
+        lnode.save()
+        if self.environment.remote_api:
+            rnode = CNode(self.name, api=self.environment.remote_api)
+            rnode[item] = value
+            rnode.save()
+
+    def save_to_node(self):
+        """
+        Save deployment restore attributes to chef environment
+        """
+        features = [str(f).lower() for f in self.features]
+        node = {'features': features,
+                'status': self.status}
+        self['archive'] = node
+
+    def apply_feature(self):
+        """
+        Runs chef client before apply features on node
+        """
+        self.status = "apply-feature"
+        if self.run_list:
+            self.run_cmd("chef-client")
+        super(ChefNode, self).apply_feature()
+
+    def add_run_list_item(self, items):
+        """
+        Adds list of items to run_list
+        """
+        util.logger.debug("run_list:{0}add:{1}".format(self.run_list, items))
+        self.run_list.extend(items)
+        cnode = CNode(self.name)
+        cnode.run_list = self.run_list
+        cnode.save()
+
+    def add_features(self, features):
+        """
+        Adds a list of feature classes
+        """
+        util.logger.debug("node:{0} feature add:{1}".format(self.name,
+                                                            features))
+        classes = {k.lower(): v for (k, v) in
+                   getmembers(node_features, isclass)}
+        for feature in features:
+            feature_class = classes[feature](self)
+            self.features.append(feature_class)
+
+        # save features for restore
+        self.save_to_node()
+
+    @classmethod
+    def from_chef_node(cls, node, os=None, product=None, environment=None,
+                       deployment=None, provisioner=None, branch=None):
+        """
+        Restores node from chef node
+        """
+        ipaddress = node['ipaddress']
+        user = node['current_user']
+        password = node['password']
+        name = node.name
+        archive = node.get('archive', {})
+        status = archive.get('status', "provisioning")
+        crnode = cls(ipaddress, user, password, os, product, environment,
+                     deployment, name, provisioner, branch, status=status)
+        try:
+            crnode.add_features(archive.get('features', []))
+        except:
+            util.logger.error(traceback.print_exc())
+            crnode.destroy()
+            raise Exception("Node feature add fail{0}".format(str(crnode)))
+        return crnode
---
+++ monster/nodes/node.py
@@ -0,0 +1,113 @@
+"""
+Provides classes of nodes (server entities)
+"""
+
+import types
+from monster import util
+from monster.server_helper import ssh_cmd, scp_to, scp_from
+
+
+class Node(object):
+    """
+    A individual computation entity to deploy a part OpenStack onto
+    Provides server related functions
+    """
+    def __init__(self, ip, user, password, os, product, environment,
+                 deployment, provisioner, status="provisioning"):
+        self.ipaddress = ip
+        self.user = user
+        self.password = password
+        self.os_name = os
+        self.product = product
+        self.environment = environment
+        self.deployment = deployment
+        self.provisoner = provisioner
+        self.features = []
+        self._cleanups = []
+        self.status = status
+
+    def __repr__(self):
+        """ Print out current instance
+        """
+        outl = 'class: ' + self.__class__.__name__
+        for attr in self.__dict__:
+            # We want to not print the deployment because
+            # it is a circular reference
+            if attr not in ['deployment', 'password']:
+                if attr == 'features':
+                    features = "\tFeatures: {0}".format(
+                        ", ".join(map(str, self.features)))
+                elif isinstance(getattr(self, attr), types.NoneType):
+                    outl += '\n\t{0} : {1}'.format(attr, 'None')
+                else:
+                    outl += '\n\t{0} : {1}'.format(attr, getattr(self, attr))
+        return "\n".join([outl, features])
+
+    def run_cmd(self, remote_cmd, user=None, password=None):
+        """
+        Runs a command on the node
+        """
+        user = user or self.user
+        password = password or self.password
+        util.logger.info("Running: {0} on {1}".format(remote_cmd, self.name))
+        return ssh_cmd(self.ipaddress, remote_cmd=remote_cmd, user=user,
+                       password=password)
+
+    def scp_to(self, local_path, user=None, password=None, remote_path=""):
+        """
+        Sends a file to the node
+        """
+        user = user or self.user
+        password = password or self.password
+        return scp_to(self.ipaddress, local_path, user=user, password=password,
+                      remote_path=remote_path)
+
+    def scp_from(self, remote_path, user=None, password=None, local_path=""):
+        """
+        Retreives a file from the node
+        """
+        user = user or self.user
+        password = password or self.password
+        return scp_from(self.ipaddress, remote_path, user=user,
+                        password=password, local_path=local_path)
+
+    def pre_configure(self):
+        """Pre configures node for each feature"""
+        self.status = "pre-configure"
+        for feature in self.features:
+            log = "Node feature: pre-configure: {0}"\
+                .format(str(feature))
+            util.logger.debug(log)
+            feature.pre_configure()
+
+    def apply_feature(self):
+        self.status = "apply-feature"
+        """Applies each feature"""
+        for feature in self.features:
+            log = "Node feature: update environment: {0}"\
+                .format(str(feature))
+            util.logger.debug(log)
+            feature.apply_feature()
+
+    def post_configure(self):
+        """Post configures node for each feature"""
+        self.status = "post-configure"
+        for feature in self.features:
+            log = "Node feature: post-configure: {0}"\
+                .format(str(feature))
+            util.logger.debug(log)
+            feature.post_configure()
+
+    def build(self):
+        """Runs build steps for node's features"""
+        self['in_use'] = ",".join(map(str, self.features))
+        self.pre_configure()
+        self.apply_feature()
+        self.post_configure()
+        self.status = "done"
+
+    def destroy(self):
+        self.status = "Destroying"
+        self.provisoner.destroy_node(self)
+        util.logger.info("Destroying node:{0}".format(self.name))
+        self.status = "Destroyed"
