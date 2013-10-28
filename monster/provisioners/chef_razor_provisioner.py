78cacc16fc091c4c0dc3add7cb4c2ff6a872bbd1
cjoelrun <cameronjlopez@gmail.com>
2013-10-28 14:12:21 -0500

    pull out config

--- monster/provisioners/chef_razor_provisioner.py
+++ monster/provisioners/chef_razor_provisioner.py
@@ -3,14 +3,13 @@ from time import sleep
 from chef import Node, Client
 
 from monster import util
-from monster.config import Config
 from monster.provisioners.provisioner import Provisioner
 from monster.razor_api import razor_api
 
 
 class ChefRazorProvisioner(Provisioner):
     def __init__(self, ip=None):
-        self.ipaddress = ip or Config()['razor']['ip']
+        self.ipaddress = ip or util.config['razor']['ip']
         self.api = razor_api(ip)
 
     def available_node(self, image, deployment):
