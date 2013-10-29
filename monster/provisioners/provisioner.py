9e476316db65a601eecf4badc015cf59be55b90c
cjoelrun <cameronjlopez@gmail.com>
2013-10-29 14:22:39 -0500

    use status

--- monster/provisioners/provisioner.py
+++ monster/provisioners/provisioner.py
@@ -39,7 +39,7 @@ class ChefRazorProvisioner(Provisioner):
         Destroys a node provisioned by razor
         """
         cnode = Node(node.name)
-        if node['in_use'] == "provisioning":
+        if node.status == "provisioning":
             # Return to pool if the node is clean
             cnode['in_use'] = 0
             cnode['archive'] = {}
