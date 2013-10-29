41cf5c2e95403f85bc493cfd8015f38ff74180e0
cjoelrun <cameronjlopez@gmail.com>
2013-10-29 18:22:02 -0500

    add xunit merge

--- monster/util.py
+++ monster/util.py
@@ -1,4 +1,8 @@
+import os
 import logging
+from glob import glob
+from xml.etree import ElementTree
+
 from inspect import getmembers, isclass
 
 # Log to console
@@ -26,3 +30,25 @@ def log_to_file(path):
 def module_classes(module):
     return {k.lower(): v for (k, v) in
             getmembers(module, isclass)}
+
+
+def xunit_merge(self, path="."):
+    print "Merging xunit files"
+    files = glob(path + "/*.xml")
+    tree = None
+    attrs = ["failures", "tests", "errors", "skip"]
+    for file in files:
+        data = ElementTree.parse(file).getroot()
+        for testcase in data.iter('testsuite'):
+            if tree is None:
+                tree = data
+                insertion_point = tree
+            else:
+                for attr in attrs:
+                    tree.attrib[attr] = str(int(tree.attrib[attr]) +
+                                            int(data.attrib[attr]))
+                insertion_point.extend(testcase)
+    if tree is not None:
+        with open("results.xunit", "w") as f:
+            f.write(ElementTree.tostring(tree))
+    [os.remove(file) for file in files]
