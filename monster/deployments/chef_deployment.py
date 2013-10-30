93e092d024ec37b2275b6629016298e9b4b59612
cjoelrun <cameronjlopez@gmail.com>
2013-10-30 16:58:44 -0500

    add features to deployment print

--- monster/deployments/chef_deployment.py
+++ monster/deployments/chef_deployment.py
@@ -27,10 +27,13 @@ class ChefDeployment(Deployment):
 
     def __str__(self):
         nodes = "\n\t".join(str(node) for node in self.nodes)
+        features = ", ".join((str(f) for f in self.features))
         deployment = ("Deployment - name:{0}, os:{1}, branch:{2}, status:{3}\n"
-                      "{4}\nNodes:\n\t{5}".format(self.name, self.os_name,
-                                                  self.branch, self.status,
-                                                  self.environment, nodes))
+                      "{4}\nFeatures{5}:\n"
+                      "Nodes:\n\t{6}".format(self.name, self.os_name,
+                                             self.branch, self.status,
+                                             self.environment, features,
+                                             nodes))
         return deployment
 
     def save_to_environment(self):
