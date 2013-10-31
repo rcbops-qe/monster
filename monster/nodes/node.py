a35fba9c871b2f62766a82d8274ebc4e2f465042
cjoelrun <cameronjlopez@gmail.com>
2013-10-30 19:57:10 -0500

    add feature names

--- monster/nodes/node.py
+++ monster/nodes/node.py
@@ -127,3 +127,6 @@ class Node(object):
                        for feature in self.features):
             return True
         return False
+
+    def feature_names(self):
+        [feature.__class__.__name__.lower() for feature in self.features]
