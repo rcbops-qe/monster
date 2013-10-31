befa1f97e90e8d27fa40383766cf4c25d47aed60
cjoelrun <cameronjlopez@gmail.com>
2013-10-30 22:08:17 -0500

    return the thing

--- monster/nodes/node.py
+++ monster/nodes/node.py
@@ -129,4 +129,5 @@ class Node(object):
         return False
 
     def feature_names(self):
-        [feature.__class__.__name__.lower() for feature in self.features]
+        return [feature.__class__.__name__.lower() for feature in
+                self.features]
