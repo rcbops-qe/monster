81c1b930c42d5c985f598b26f94ce9344c534b7c
cjoelrun <cameronjlopez@gmail.com>
2013-10-30 19:56:54 -0500

    use feature names

--- monster/nodes/chef_node.py
+++ monster/nodes/chef_node.py
@@ -25,7 +25,7 @@ class ChefNode(Node):
         self.features = []
 
     def __str__(self):
-        features = ", ".join((str(f) for f in self.features))
+        features = ", ".join(self.feature_names)
         node = ("Node - name:{0}, os:{1}, branch:{2}, ip:{3}, status:{4}\n\t\t"
                 "Features: {5}").format(self.name, self.os_name, self.branch,
                                         self.ipaddress, self.status, features)
