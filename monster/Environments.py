f3f0e4ed65d011595b8271d7250a70261dde634e
Jacob Wagner <swagner1104@gmail.com>
2014-01-06 15:39:27 -0600

    removed unneeded folders that had nothing to do with the monster project or needed to be implemented in the automation that were implemented

--- monster/Environments.py
+++ monster/Environments.py
@@ -13,8 +13,10 @@ class Environment(dict):
         self.description = description
 
     def __repr__(self):
-        """ Print out current instance
+        """ 
+        Print out current instance
         """
+        
         outl = 'class: ' + self.__class__.__name__
         for attr in self.__dict__:
             outl += '\n\t' + attr + ' : ' + str(getattr(self, attr))
