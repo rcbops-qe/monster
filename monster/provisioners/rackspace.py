357d0128a1bd4b3683ec270a7663ca87ee459e0c
Jacob Wagner <swagner1104@gmail.com>
2014-01-09 15:55:00 -0600

    argh

---
+++ monster/provisioners/rackspace.py
@@ -0,0 +1,64 @@
+import pyrax
+
+from monster import util
+from openstack import Openstack
+from monster.clients.openstack import Creds
+
+class Rackspace(Openstack):
+    """
+    Provisions chef nodes in Rackspace Cloud Servers vms
+    """
+
+    def __init__(self):
+        rackspace = util.config['secrets']['rackspace']
+
+        self.names = []
+        self.name_index = {}
+        self.creds = Creds(
+            user=rackspace['user'], apikey=rackspace['api_key'],
+            auth_url=rackspace['auth_url'], region=rackspace['region'],
+            auth_system=rackspace['plugin'], provisioner="rackspace")
+        
+        pyrax.set_setting("identity_type", "rackspace")
+        pyrax.set_credentials(self.creds.user, api_key=self.creds.apikey,
+                              region=self.creds.region)
+        pyrax.connect_to_services()
+        
+        self.client = pyrax.cloudservers
+        self.neutron = pyrax.cloud_networks
+
+    def post_provision(self, node):
+        """
+        Tasks to be done after a rackspace node is provisioned
+        :param node: Node object to be tasked
+        :type node: Monster.Node
+        """
+        self.mkswap(node)
+        self.clean(node)
+
+    def clean(self, node):
+        # remove /etc/hosts entries
+        cmd = ("sed '/{0}/d' /etc/hosts > /etc/hosts; "
+               "echo '127.0.0.1 localhost' >> /etc/hosts".format(node.name))
+        node.run_cmd(cmd)
+
+    def mkswap(self, node, size=2):
+        """
+        Makes a swap file of size on the node
+        :param node: Node to create swap file
+        :type node: monster.Node
+        :param size: Size of swap file in GBs
+        :type size: int
+        """
+        util.logger.info("Making swap file on:{0} of {1}GBs".format(node.name,
+                                                                    size))
+        size_b = int(pow(1024, size))
+        cmds = [
+            "dd if=/dev/zero of=/mnt/swap bs=1024 count={0}".format(size_b),
+            "mkswap /mnt/swap",
+            "sed 's/vm.swappiness.*$/vm.swappiness=25/g' "
+            "/etc/sysctl.conf > /etc/sysctl.conf",
+            "sysctl vm.swappiness=30",
+            "swapon /mnt/swap"
+        ]
+        node.run_cmd("; ".join(cmds))
---
+++ monster/provisioners/util.py
@@ -0,0 +1,18 @@
+import sys
+from monster.provisioners import *
+from monster.util import module_classes
+
+def get_provisioner(provisioner):
+    """
+    This will return an instance of the correct provisoner class
+    :param provisioner: The name of the provisoner
+    :type provisioner: String
+    :rtype: object
+    """
+
+    try:
+        identifier = getattr(sys.modules['monster'].provisioners, provisioner)
+    except AttributeError:
+        raise NameError("{0} doesn't exist.".format(provisioner))
+
+    return module_classes(identifier)[provisioner]()
