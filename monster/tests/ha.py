1b11dfbc73372c67a25bdd24d07d5236d05e5376
xeregin <xeregin@gmail.com>
2014-03-01 02:00:45 +0000

    flake8'ed ha.py

--- monster/tests/ha.py
+++ monster/tests/ha.py
@@ -51,84 +51,112 @@ class HATest(Test):
             tempest = TempestNeutron(self.deployment)
         tempest.test_node = self.controller2
 
-        netns_value=tempest.test_node.run_cmd("ip netns exec vips ip a")
-        netns_value=netns_value['return']
+        netns_value = tempest.test_node.run_cmd("ip netns exec vips ip a")
+        netns_value = netns_value['return']
         print "RETVALUE ip netns exec vips ip a: {0}".format(netns_value)
 
+        srcmd = "source openrc"
+        cmd1 = ("neutron net-create testnet | sed '/^| id/!d' | "
+                "awk '{{ print $4 }}'")
+        neutron_net_create_value = tempest.test_node.run_cmd(";".join([srcmd,
+                                                                       cmd1]))
+        neutron_net_create_value = neutron_net_create_value['return'].rstrip()
+        print ("RETVALUE neutron net-create testnet: "
+               "{0}".format(neutron_net_create_value))
 
-        neutron_net_create_value=tempest.test_node.run_cmd(";".join(["source openrc", "neutron net-create testnet | sed '/^| id/!d' | awk '{{ print $4 }}'"]))
-        neutron_net_create_value=neutron_net_create_value['return'].rstrip()
-        print "RETVALUE neutron net-create testnet: {0}".format(neutron_net_create_value)
+        cmd1 = ("neutron subnet-create --name testsubnet {0} 172.32.0.0/24 "
+                "--no-gateway | sed '/^| id/!d' | "
+                "awk '{{ print $4 }}'".format(neutron_net_create_value))
+        neutron_subnet_create_value = tempest.test_node.run_cmd(";".join(
+                                                                [srcmd,
+                                                                 cmd1]))
+        neutron_subnet_create_value = neutron_subnet_create_value['return']
+        neutron_subnet_create_value = neutron_subnet_create_value.rstrip()
+        print ("RETVALUE neutron subnet-create subtestnet: "
+               "{0}".format(neutron_subnet_create_value))
 
-
-        neutron_subnet_create_value=tempest.test_node.run_cmd(";".join(["source openrc", "neutron subnet-create --name testsubnet {0} 172.32.0.0/24 --no-gateway | sed '/^| id/!d' | awk '{{ print $4 }}'".format(neutron_net_create_value)]))
-        neutron_subnet_create_value=neutron_subnet_create_value['return'].rstrip()
-        print "RETVALUE neutron subnet-create subtestnet: {0}".format(neutron_subnet_create_value)
-
-
-        neutron_net_list_value=tempest.test_node.run_cmd(";".join(["source openrc", "neutron net-list | grep {0} | grep {1}".format(neutron_net_create_value, neutron_subnet_create_value)]))
-        neutron_net_list_value=neutron_net_list_value['return'].rstrip()
+        cmd1 = ("neutron net-list | grep {0} | "
+                "grep {1}".format(neutron_net_create_value,
+                                  neutron_subnet_create_value))
+        neutron_net_list_value = tempest.test_node.run_cmd(";".join([srcmd,
+                                                                     cmd1]))
+        neutron_net_list_value = neutron_net_list_value['return'].rstrip()
         print "RETVALUE neutron net-list: {0}".format(neutron_net_list_value)
         if not neutron_net_list_value:
             print "Network or Subnet FAILED TO INITIALIZE!"
             #pass
 
 #--nic requires the Value for the field "id" from neutron net-create testnet
-        nova_boot_value=tempest.test_node.run_cmd(";".join(["source openrc", "nova boot --image cirros-image --flavor 1 --nic net-id={0} testbuild | sed '/^| id/!d' | awk '{{ print $4 }}'".format(neutron_net_create_value)]))
-        nova_boot_value=nova_boot_value['return'].rstrip()
+        cmd1 = ("nova boot --image cirros-image --flavor 1 --nic net-id={0} "
+                "testbuild | sed '/^| id/!d' | "
+                "awk '{{ print $4 }}'".format(neutron_net_create_value))
+        nova_boot_value = tempest.test_node.run_cmd(";".join([srcmd,
+                                                              cmd1]))
+        nova_boot_value = nova_boot_value['return'].rstrip()
         print "RETVALUE nova boot: {0}".format(nova_boot_value)
 
-
+        cmd1 = ("nova list | grep {0} | "
+                "awk '{{ print $6 }}'".format(nova_boot_value))
         build_status = "BUILD"
         while build_status == "BUILD":
-            build_status = tempest.test_node.run_cmd(";".join(["source openrc", "nova list | grep {0} | awk '{{ print $6 }}'".format(nova_boot_value)]))
-            build_status=build_status['return'].rstrip()
+            build_status = tempest.test_node.run_cmd(";".join([srcmd,
+                                                               cmd1]))
+            build_status = build_status['return'].rstrip()
 
         if build_status == "ERROR":
             print "Build FAILED TO INITIALIZE!"
             #pass
 
-        ip_netns_value=tempest.test_node.run_cmd("ip netns")
-        ip_netns_value=ip_netns_value['return'].rstrip()
+        ip_netns_value = tempest.test_node.run_cmd("ip netns")
+        ip_netns_value = ip_netns_value['return'].rstrip()
         print "RETVALUE ip netns: {0}".format(ip_netns_value)
 
-
-        current_host=tempest.test_node.run_cmd("hostname")
-        current_host=current_host['return'].rstrip()
+        current_host = tempest.test_node.run_cmd("hostname")
+        current_host = current_host['return'].rstrip()
         print "RETVALUE hostname: {0}".format(current_host)
 
-
-        neutron_dhcp_value=tempest.test_node.run_cmd(";".join(["source openrc", "neutron dhcp-agent-list-hosting-net testnet | grep {0} | awk '{{ print $6 }}'".format(current_host)]))
-        neutron_dhcp_value=neutron_dhcp_value['return'].rstrip()
-        print "RETVALUE neutron dhcp-agent-list-hosting-net testnet: {0}".format(neutron_dhcp_value)
+        cmd1 = ("neutron dhcp-agent-list-hosting-net testnet | "
+                "grep {0} | awk '{{ print $6 }}'".format(current_host))
+        neutron_dhcp_value = tempest.test_node.run_cmd(";".join([srcmd,
+                                                                 cmd1]))
+        neutron_dhcp_value = neutron_dhcp_value['return'].rstrip()
+        print ("RETVALUE neutron dhcp-agent-list-hosting-net testnet: "
+               "{0}".format(neutron_dhcp_value))
         if neutron_dhcp_value == "False":
             print "DHCP is NOT WORKING PROPERLY ON THE CURRENT HOST!"
             #pass
 
-        tempest.test_node.run_cmd(";".join(["source openrc", "nova delete testbuild", "neutron net-delete {0}".format(neutron_net_create_value)]))
+        cmd1 = "nova delete testbuild"
+        cmd2 = "neutron net-delete {0}".format(neutron_net_create_value)
+        tempest.test_node.run_cmd(";".join([srcmd, cmd1, cmd2]))
         print "RETVALUE DESTROYING INSTANCE AND NETWORK"
 
-
-        rabbit_value=tempest.test_node.run_cmd("rabbitmqctl list_queues 2>&1 >/dev/null | grep Error")['return'].rstrip()
+        cmd1 = "rabbitmqctl list_queues 2>&1 >/dev/null | grep Error"
+        rabbit_value = tempest.test_node.run_cmd(cmd1)['return'].rstrip()
         print "RabbitMQ should be running!"
         if rabbit_value:
-            print "RabbitMQ is not functioning properly!: {0}".format(rabbit_value)
+            print ("RabbitMQ is not functioning properly!: "
+                   "{0}".format(rabbit_value))
         else:
             print "RabbitMQ is functioning properly!"
 
         tempest.test_node.run_cmd("service rabbitmq-server stop")
-        rabbit_value=tempest.test_node.run_cmd("rabbitmqctl list_queues 2>&1 >/dev/null | grep Error")['return'].rstrip()
+        cmd1 = "rabbitmqctl list_queues 2>&1 >/dev/null | grep Error"
+        rabbit_value = tempest.test_node.run_cmd(cmd1)['return'].rstrip()
         print "RabbitMQ should NOT be running now!"
         if rabbit_value:
-            print "RabbitMQ test is working as expected. RabbitMQ is down!: {0}".format(rabbit_value)
+            print ("RabbitMQ test is working as expected. RabbitMQ is down!: "
+                   "{0}".format(rabbit_value))
         else:
             print "RabbitMQ test has failed to identify a down issue!"
 
         tempest.test_node.run_cmd("service rabbitmq-server start")
-        rabbit_value=tempest.test_node.run_cmd("rabbitmqctl list_queues 2>&1 >/dev/null | grep Error")['return'].rstrip()
+        cmd1 = "rabbitmqctl list_queues 2>&1 >/dev/null | grep Error"
+        rabbit_value = tempest.test_node.run_cmd(cmd1)['return'].rstrip()
         print "RabbitMQ should be running!"
         if rabbit_value:
-            print "RabbitMQ is not functioning properly!: {0}".format(rabbit_value)
+            print ("RabbitMQ is not functioning properly!: "
+                   "{0}".format(rabbit_value))
         else:
             print "RabbitMQ is functioning properly!"
 
