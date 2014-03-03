ca15ad0d6a611ce4d25179f6553836751de3563f
cjoelrun <cameronjlopez@gmail.com>
2014-03-03 00:43:26 +0000

    add module docstring

--- tools/ipython.py
+++ tools/ipython.py
@@ -1,3 +1,13 @@
+"""
+Tool to load deployments in iPython
+
+Use:
+1. Start ipython in top monster directory
+2. from tools.ipython import load
+3. deployment = load("yourdep")
+4. profit???
+"""
+
 from monster import util
 from monster.config import Config
 from monster.deployments.chef_deployment import Chef
