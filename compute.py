909be6330f055373a5cb1c5c277c5beffebb605d
James Courtois <jcourtois@thoughtworks.com>
2014-03-18 16:56:28 -0500

    james | moving things around...

--- compute.py
+++ compute.py
@@ -4,12 +4,15 @@
 Command Line interface for Building Openstack clusters
 """
 
+from monster import util
+from IPython import embed
 try:
-    import argh
     import subprocess
     import traceback
     import webbrowser
-    from monster import util
+    import os
+    import inspect
+    from compute_cli import CLI
     from monster.config import Config
     from monster.tests.ha import HATest
     from monster.provisioners.util import get_provisioner
@@ -17,46 +20,71 @@ try:
     from monster.tests.tempest_quantum import TempestQuantum
     from monster.deployments.chef_deployment import Chef as ChefDeployment
 except ImportError as error:
-    print("ERROR: There was an import error when trying to load '{0}'. This "
-          "may be resolved if you load the monster virtual environment with "
-          "the command \"source .venv/bin/activate\""
-          .format(error.message[16:]))
+    util.logger.error("There was an import error when trying to load '{0}' "
+                      "This may be resolved if you load the monster virtual"
+                      " environment with the command \"source .venv/bin/"
+                      "activate\"".format(error.message[16:]))
     exit(1)
+if 'monster' not in os.environ.get('VIRTUAL_ENV', ''):
+    util.logger.warning("You are not using the virtual environment! We "
+                        "cannot guarantee that your monster will be well"
+                        "-behaved.  To load the virtual environment, use "
+                        "the command \"source .venv/bin/activate\"")
+
+def __log(function):
+    def wrap_function(args):
+        util.logger.setLevel(args.log_level)
+        util.log_to_file(args.logfile_path)
+        return function(args)
+    return wrap_function
+
+def __load_deployment(function):
+    def wrap_function(args):
+        util.config = Config(args.config, args.secret_path)
+        deployment = ChefDeployment.from_chef_environment(args.name)
+        util.logger.debug("Loading deployment {0}".format(deployment))
+        return function(deployment, args)
+    return wrap_function
+
+def __build_deployment(function):
+    def wrap_function(args):
+        util.logger.info("Building deployment object for %s" % args.name)
+        util.logger.debug("Creating ChefDeployment with dict %s" % args)
+        try:
+            args.deployment = ChefDeployment.fromfile(**vars(args))
+        except TypeError as error:
+            util.logger.critical(
+                str(error) +
+                "ChefDeployment.fromfile was called with \n{0},\n but "
+                "expects at least the following non-none : {1}."
+                .format(vars(args),
+                        inspect.getargspec(ChefDeployment.fromfile)[0][1:]))
+            exit(1)
+        return function(args)
+    return wrap_function
+
+def __provision_for_deployment(function):
+    def wrap_function(args):
+        util.config = Config(args.config, args.secret_path)
+        args.provisioner=get_provisioner(args.provisioner)
+        return function(args)
+    return wrap_function
 
 
-def build(name="autotest", template="ubuntu-default", branch="master",
-          config=None, dry=False, log=None, log_level="INFO",
-          provisioner="rackspace", secret_path=None):
+@__log
+@__provision_for_deployment
+@__build_deployment
+def build(deployment, dry):
     """
     Builds an OpenStack Cluster
     """
-    _set_log(log, log_level)
-
-    # Magic to get the template location from the branch
-    if branch == "master":
-        template_file = "default"
-    else:
-        temp_branch = branch.lstrip('v')
-        if "rc" in temp_branch:
-            template_file = temp_branch.rstrip("rc").replace('.', '_')
-        else:
-            template_file = temp_branch.replace('.', '_')
-
-    # provision deployment
-    util.config = Config(config, secret_path=secret_path)
-    cprovisioner = get_provisioner(provisioner)
-
-    util.logger.info("Building deployment object for {0}".format(name))
-    deployment = ChefDeployment.fromfile(
-        name, template, branch, cprovisioner, template_file)
-
     if dry:
         try:
             deployment.update_environment()
-        except Exception:
+        except Exception: # are you kidding me???
             error = traceback.print_exc()
             util.logger.error(error)
-            raise
+            exit(1)
 
     else:
         util.logger.info(deployment)
@@ -65,55 +93,17 @@ def build(name="autotest", template="ubuntu-default", branch="master",
         except Exception:
             error = traceback.print_exc()
             util.logger.error(error)
-            raise
+            exit(1)
 
     util.logger.info(deployment)
 
 
-def retrofit(name='autotest', retro_branch='dev', ovs_bridge='br-eth1',
-             x_bridge='lxb-mgmt', iface='eth0', del_port=None, config=None,
-             log=None, log_level='INFO', secret_path=None):
-
-    """
-    Retrofit a deployment
-    """
-    _set_log(log, log_level)
-    deployment = _load(name, config, secret_path)
-    util.logger.info(deployment)
-    deployment.retrofit(retro_branch, ovs_bridge, x_bridge, iface, del_port)
-
-
-def upgrade(name='autotest', upgrade_branch='v4.1.3rc',
-            config=None, log=None, log_level="INFO", secret_path=None):
-    """
-    Upgrades a current deployment to the new branch / tag
-    """
-    _set_log(log, log_level)
-    deployment = _load(name, config, secret_path)
-    util.logger.info(deployment)
-    deployment.upgrade(upgrade_branch)
-
-
-def destroy(name="autotest", config=None, log=None, log_level="INFO",
-            secret_path=None):
-    """
-    Destroys an existing OpenStack deployment
-    """
-    _set_log(log, log_level)
-    deployment = _load(name, config, secret_path)
-    util.logger.info(deployment)
-    deployment.destroy()
-
-
-def test(name="autotest", config=None, log=None, log_level="INFO",
-         tempest=False, ha=False, secret_path=None, deployment=None,
-         iterations=1, provider_net="6241dfe9-11fe-45e7-b39d-45ef88f5d9cb"):
+@__log
+@__load_deployment
+def test(deployment, tempest=False, ha=False, iterations=1):
     """
     Tests an openstack deployment
     """
-    if not deployment:
-        _set_log(log, log_level)
-        deployment = _load(name, config, secret_path)
     if not tempest and not ha:
         tempest = True
         ha = True
@@ -166,76 +156,83 @@ def getFile(ip, user, password, remote, local, remote_delete=False):
         subprocess.call(cmd2, shell=True)
 
 
-def artifact(name="autotest", config=None, log=None, secret_path=None,
-             log_level="INFO"):
+@__log
+@__load_deployment
+def retrofit(deployment, retro_branch='dev', ovs_bridge='br-eth1',
+             x_bridge='lxb-mgmt', iface='eth0', del_port=None):
+    """
+    Retrofit a deployment
+    """
+    deployment.retrofit(retro_branch, ovs_bridge, x_bridge, iface, del_port)
+
+
+@__log
+@__load_deployment
+def upgrade(deployment, args): #do we really want to set this default (no!!)
+    """
+    Upgrades a current deployment to the new branch / tag
+    """
+    deployment.upgrade(args['upgrade_branch'])
+
+@__log
+@__load_deployment
+def destroy(deployment, args):
+    """
+    Destroys an existing OpenStack deployment
+    """
+    deployment.destroy()
+
+
+@__log
+@__load_deployment
+def artifact(deployment, args):
     """
     Artifacts a deployment (configs / running services)
     """
-
-    _set_log(log, log_level)
-    deployment = _load(name, config, secret_path)
     deployment.artifact()
 
 
-def openrc(name="autotest", config=None, log=None, secret_path=None,
-           log_level="INFO"):
+@__log
+@__load_deployment
+def openrc(deployment, args):
     """
     Loads OpenStack credentials into shell env
     """
-    _set_log(log, log_level)
-    deployment = _load(name, config, secret_path)
     deployment.openrc()
 
 
-def tmux(name="autotest", config=None, log=None, secret_path=None,
-         log_level="INFO"):
+@__log
+@__load_deployment
+def tmux(deployment, args):
     """
     Loads OpenStack nodes into new tmux session
     """
-    _set_log(log, log_level)
-    deployment = _load(name, config, secret_path)
     deployment.tmux()
 
 
-def horizon(name="autotest", config=None, log=None, secret_path=None,
-            log_level="INFO"):
+@__log
+@__load_deployment
+def horizon(deployment, args):
     """
     Opens horizon in a browser tab
     """
-    _set_log(log, log_level)
-    deployment = _load(name, config, secret_path)
     ip = deployment.horizon_ip()
-    url = "https://{0}".format(ip)
+    url = "https://{0}".format(ip)  # i don't think this will work
     webbrowser.open_new_tab(url)
 
 
-def show(name="autotest", config=None, log=None, secret_path=None,
-         log_level="INFO"):
+@__log
+@__load_deployment
+def show(deployment, args):
     """
-    Shows details about and OpenStack deployment
+    Shows details about an OpenStack deployment
     """
-    _set_log(log, log_level)
-    # load deployment and source openrc
-    deployment = _load(name, config, secret_path)
     util.logger.info(str(deployment))
 
-
-def _load(name="autotest", config=None, secret_path=None):
-    # load deployment and source openrc
-    util.config = Config(config, secret_path=secret_path)
-    return ChefDeployment.from_chef_environment(name)
-
-
-def _set_log(log, log_level):
-    # set log level and file
-    util.set_log_level(log_level)
-    if log:
-        util.log_to_file(log)
-
+# is artifact supposed to be in the CLI?
+args=CLI.parser({'build':build,'destroy':destroy,'horizon':horizon,
+                 'openrc':openrc,'retrofit':retrofit,'show':show,
+                 'test':test,'tmux':tmux,'upgrade':upgrade}).parse_args()
 
 if __name__ == "__main__":
-    parser = argh.ArghParser()
-    parser.add_commands([build, retrofit, upgrade,
-                        destroy, openrc, horizon,
-                        show, test, tmux])
-    parser.dispatch()
+        args.func(args)
---
+++ compute_cli.py
@@ -0,0 +1,70 @@
+#! /usr/bin/env python
+
+from IPython import embed
+import argparse
+class CLI:
+    @classmethod
+    def parser(self, commands):
+        parser=argparse.ArgumentParser(
+            description="Monster deployment and testing tool. Developed"
+                        " for Rackspace Private Cloud QE.")
+        subparsers = parser.add_subparsers()
+
+        build_parser=subparsers.add_parser('build')
+        build_parser.add_argument('-t', '--template-name', default='ubuntu-ha')
+        build_parser.add_argument('-T', '--template-file', default='default')
+        build_parser.add_argument('-b', '--branch', default='master')
+        build_parser.add_argument('-p', '--provisioner', default='rackspace')
+        build_parser.add_argument('-d', '--dry', action='store_true')
+        build_parser.set_defaults(func=commands['build'])
+
+        retrofit_parser=subparsers.add_parser('retrofit')
+        retrofit_parser.add_argument('-r', '--retro-branch')
+        retrofit_parser.add_argument('-o', '--ovs-brige')
+        retrofit_parser.add_argument('-x', '--x-bridge')
+        retrofit_parser.add_argument('-i', '--interface')
+        retrofit_parser.add_argument('-d', '--delete-port')
+        retrofit_parser.set_defaults(func=commands['retrofit'])
+
+        destroy_parser=subparsers.add_parser('destroy')
+        destroy_parser.set_defaults(func=commands['destroy'])
+
+        upgrade_parser=subparsers.add_parser('upgrade')
+        upgrade_parser.add_argument('-u', '--upgrade_branch',
+                                    choices=['v4.1.3rc','v4.2.2rc'],
+                                    required=True)
+        upgrade_parser.set_defaults(func=commands['upgrade'])
+
+        openrc_parser=subparsers.add_parser('openrc')
+        openrc_parser.set_defaults(func=commands['openrc'])
+
+        horizon_parser=subparsers.add_parser('horizon')
+        horizon_parser.set_defaults(func=commands['horizon'])
+
+        show_parser=subparsers.add_parser('show')
+        show_parser.set_defaults(func=commands['show'])
+
+        test_parser=subparsers.add_parser('test')
+        test_parser.add_argument('-d', '--deployment')
+        test_parser.add_argument('-i', '--iterations', default=1)
+        test_to_run=test_parser.add_mutually_exclusive_group(required=True)
+        test_to_run.add_argument('-a', '--all')
+        test_to_run.add_argument('--HA', '--ha')
+        test_to_run.add_argument('--tp', '--tempest')
+        test_to_run.add_argument('--oc', '--opencafe')
+        test_parser.set_defaults(func=commands['test'])
+
+        tmux_parser=subparsers.add_parser('tmux')
+        tmux_parser.set_defaults(func=commands['tmux'])
+
+        def add_common_arguments(parser):
+            parser.add_argument('-n', '--name')
+            parser.add_argument('-c', '--config', default='rspc.yaml')
+            parser.add_argument('-s', '--secret-path', default='secret.yaml')
+            parser.add_argument('-l', '--logfile-path', default='./log')
+            parser.add_argument('-L', '--log-level',
+                                choices=['DEBUG', 'INFO', 'WARNING',
+                                         'ERROR', 'CRITICAL'],
+                                default='DEBUG')
+        map(add_common_arguments, subparsers.choices.values())
+        return parser
