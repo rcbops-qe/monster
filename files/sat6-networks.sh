e641e419f3be2715ce4fa3cae33b2a3a55c2776b
Cameron Lopez <cameronjlopez@gmail.com>
2014-03-25 11:40:52 -0500

    hide the bash from jacob

--- files/sat6-networks.sh
+++ files/sat6-networks.sh
@@ -1,3 +1,7 @@
+#!/bin/bash
+
+set -x
+
 #add a key called key
 
 for i in `neutron security-group-list | awk '/default/ {print $2}'`; do neutron security-group-rule-create --protocol tcp --port-range-min 22 --port-range-max 22 --direction ingress $i; done
@@ -8,9 +12,23 @@ neutron subnet-create ENV01-FLAT 10.127.100.0/24 --name ENV01-FLAT-SUBNET --no-g
 
 nova boot --flavor 2 --image precise-image --security-groups default --key-name key --nic net-id=$flatnetid flattest
 
-vlannetid=`neutron net-create ENV01-VLAN --provider:network_type=vlan --provider:physical_network=ph-vmnet --provider:segmentation_id=867 | grep -w id | awk '{print $4}'`
-neutron subnet-create ENV01-VLAN 10.127.102.0/24 --name ENV01-VLAN-SUBNET --gateway 10.127.102.1 --allocation-pool start=10.127.102.128,end=10.127.102.159 --dns-nameservers list=true 8.8.8.8 8.8.4.4
-neutron net-update $vlannetid --router:external=true
+vlan_provider() {
+  vlannetid=`neutron net-create ENV01-VLAN --provider:network_type=vlan --provider:physical_network=ph-vmnet --provider:segmentation_id=867 | grep -w id | awk '{print $4}'`
+  neutron subnet-create ENV01-VLAN 10.127.102.0/24 --name ENV01-VLAN-SUBNET --gateway 10.127.102.1 --allocation-pool start=10.127.102.128,end=10.127.102.159
+  neutron net-update $vlannetid --router:external=true
+}
+
+vlan_tenant() {
+  vlannetid=`neutron net-create ENV01-VLAN --provider:network_type=vlan --provider:physical_network=ph-vmnet --provider:segmentation_id=867 | grep -w id | awk '{print $4}'`
+  neutron subnet-create ENV01-VLAN 10.127.102.0/24 --name ENV01-VLAN-SUBNET --no-gateway --host-route destination=0.0.0.0/0,nexthop=10.127.102.1 --allocation-pool start=10.127.102.128,end=10.127.102.159 --dns-nameservers list=true 8.8.8.8 8.8.4.4
+  nova boot --flavor 2 --image precise-image --security-groups default --key-name key --nic net-id=$vlannetid vlantest
+}
+
+if $1 == "provider"; then
+    vlan_provider
+elif $1 == "tenant"; then
+    vlan_tenant
+fi
 
 grenetid=`neutron net-create ENV01-GRE --provider:network_type=gre --provider:segmentation_id=1 | grep -w id | awk '{print $4}'`
 gresubid=`neutron subnet-create ENV01-GRE 192.168.105/24 --name ENV01-GRE-SUBNET --dns-nameservers list=true 8.8.8.8 8.8.4.4 | grep -w id | awk '{print $4}'`
