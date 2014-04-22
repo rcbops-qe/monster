36b75eb3aeacda2c3236f0dd0c111f262689834d
James Courtois <jcourtois@thoughtworks.com>
2014-04-22 08:56:57 -0500

    james | moved unnecessary functions from feature module (this functionality now exists on node objects themselves)

--- monster/features/feature.py
+++ monster/features/feature.py
@@ -1,6 +1,4 @@
-"""
-Base Feature
-"""
+""" Base Feature """
 
 
 class Feature(object):
@@ -40,79 +38,3 @@ class Feature(object):
 
     def archive(self):
         pass
-
-
-def remove_chef(node):
-    """
-    Removes chef from the given node
-
-    :param node: Node object to remove chef from
-    :type node: basenode.BaseNode
-    :rtype: function
-    """
-    if node.os_name == "ubuntu":
-        commands = ["apt-get remove --purge -y chef",
-                    "rm -rf /etc/chef"]
-    if node.os_name in ["centos", "rhel"]:
-        commands = ["yum remove -y chef",
-                    "rm -rf /etc/chef /var/chef"]
-    command = "; ".join(commands)
-
-    return node.run_cmd(command)
-
-
-def install_package(node, package):
-    """
-    Installs given package
-
-    :param package: package to install
-    :type package: String
-    :rtype: function
-    """
-
-    # Need to make this more machine agnostic (jwagner)
-    if node.os_name == "precise":
-        command = 'apt-get install -y {0}'.format(package)
-    if node.os_name in ["centos", "rhel"]:
-        command = 'yum install -y {0}'.format(package)
-
-    return node.run_cmd(command)
-
-
-def install_packages(node, packages):
-    """
-    Installs a list of packages
-
-    :param packages: List of packages to install
-    :type packages: list of Strings
-    """
-
-    for package in packages:
-        install_package(node, package)
-
-
-def install_ruby_gem(node, gem):
-    """
-    Installs a ruby gem
-
-    :param gem: Ruby gem to install
-    :type gem: str
-    :rtype: function
-    """
-
-    command = ('source /usr/local/rvm/scripts/rvm; gem install '
-               '--no-rdoc --no-ri {0}'.format(gem))
-
-    return node.run_cmd(command)
-
-
-def install_ruby_gems(node, gems):
-    """
-    Installs a list of ruby gems
-
-    :param gems: List of gems to install
-    :type gems: list of Strings
-    """
-
-    for gem in gems:
-        install_ruby_gem(node, gem)
