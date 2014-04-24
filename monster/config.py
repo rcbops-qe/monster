c94f6733bd3bd4c6cf5d482304593b8ca5b6ef64
James Courtois <jcourtois@thoughtworks.com>
2014-04-24 09:57:10 -0500

    james | adding null guard to secret path.

--- monster/config.py
+++ monster/config.py
@@ -13,14 +13,14 @@ logger = logging.getLogger(__name__)
 
 class Config(object):
     """Application config object"""
-    def __init__(self, config, secret_path=None):
+    def __init__(self, config, secret):
         template_path = path.join(path.dirname(path.dirname(__file__)), config)
         with open(template_path, 'r') as template:
             self.config = defaultdict(None, load(template.read()))
 
-        if secret_path:
-            with open(secret_path, 'r') as secret:
-                self.config['secrets'] = load(secret.read())
+        secret_path = secret or "secret.yaml"
+        with open(secret_path, 'r') as secret:
+            self.config['secrets'] = load(secret.read())
 
     def __getitem__(self, name):
         return self.config[name]
