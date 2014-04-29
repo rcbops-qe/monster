8caab67e9e204e3a56fe0a43b8a9cd38a9abff31
James Courtois <jcourtois@thoughtworks.com>
2014-04-29 11:16:46 -0500

    james | turning config from a class to a module with methods.

--- monster/config.py
+++ monster/config.py
@@ -1,5 +1,3 @@
-"""Gathers application config"""
-
 import logging
 
 from os import path
@@ -11,44 +9,41 @@ from monster.template import Template
 logger = logging.getLogger(__name__)
 
 
-class Config(object):
-    def __init__(self, config, secret=None):
-        """Initializes application config object.
-        :param config: configuration files, stored in configs directory.
-        :param secret: secret path, stored in project root.
-        """
-        template_path = path.join(path.dirname(path.dirname(__file__)), config)
-        with open(template_path, 'r') as template:
-            self.config = defaultdict(None, load(template.read()))
+def fetch_config(config, secret=None):
+    """Returns a dictionary with the deployment's config loaded in it.
+    :param config: configuration files, stored in configs directory.
+    :param secret: secret path, stored in project root.
+    """
+    template_path = path.join(path.dirname(path.dirname(__file__)), config)
+    with open(template_path, 'r') as template:
+        config = defaultdict(None, load(template.read()))
 
-        secret_path = secret or "secret.yaml"
-        with open(secret_path, 'r') as secret:
-            self.config['secrets'] = load(secret.read())
+    secret_path = secret or "secret.yaml"
+    with open(secret_path, 'r') as secret:
+        config['secrets'] = load(secret.read())
+    return config
 
-    def __getitem__(self, name):
-        return self.config[name]
 
-    @classmethod
-    def fetch_template(cls, template_name, branch):
-        """
-        :param template_name
-        :param branch
-        :return: Template
-        """
-        if branch == "master":
-            template_file = "default"
-        else:
-            template_file = branch.lstrip('v').rstrip("rc").replace('.', '_')
+def fetch_template(template_name, branch):
+    """Returns a dictionary with the deployment's template loaded in it.
+    :param template_name
+    :param branch
+    :return: Template
+    """
+    if branch == "master":
+        template_file = "default"
+    else:
+        template_file = branch.lstrip('v').rstrip("rc").replace('.', '_')
 
-        template_path = path.join(path.dirname(__file__), path.pardir,
-                                  "templates/{0}.yaml".format(template_file))
+    template_path = path.join(path.dirname(__file__), path.pardir,
+                              "templates/{0}.yaml".format(template_file))
 
-        try:
-            template = Template(Config(template_path)[template_name])
-        except KeyError:
-            logger.critical("Looking for the template {0} in the file: "
-                            "\n{1}\n The key was not found!"
-                            .format(template_file, template_path))
-            exit(1)
-        else:
-            return template
+    try:
+        template = Template(fetch_config(template_path)[template_name])
+    except KeyError:
+        logger.critical("Looking for the template {0} in the file: "
+                        "\n{1}\n The key was not found!"
+                        .format(template_file, template_path))
+        exit(1)
+    else:
+        return template
