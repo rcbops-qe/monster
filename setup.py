9655741a19d1c50929d35ac8303cd963d0bd25de
James Courtois <jcourtois@thoughtworks.com>
2014-05-01 16:15:14 -0500

    james | adding package_dir metadata

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setuptools.setup(
     name="monster",
     version="0.1.0",
     description="An OpenStack Deployment/Orchestration Engine",
-    install_dir=os.getcwd(),
+    package_dir=os.getcwd(),
     packages=setuptools.find_packages(),
     entry_points={
         "console_scripts": ["monster = monster.executable:run"]}
